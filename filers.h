4ec642f087fb912358c709e5b736ca4450b59aa7
unknown <jon.smit@gmail.com>
2019-09-07 15:02:37 +0300

    split filters

---
+++ filers.h
@@ -0,0 +1,137 @@
+#ifndef _fillers_h_
+#define _fillers_h_
+#endif
+	struct Filter
+	{
+		std::deque<dataType> process(const std::deque<dataType> & signal) = 0;
+		size_t delay() = 0;
+	};
+
+	struct DCFilter : public Filter	{ };
+	struct LowPassFilter : public Filter { };
+	struct HighPassFilter : public Filter { };
+	struct DerivativeFilter : public Filter { };
+	struct SquaredFilter : public Filter { };
+	struct MWI : public Filter { };
+
+	void normalize(std::deque<dataType> & values)
+	{
+		dataType max_value = values[0];
+		for (size_t index = 1; index < values.size(); ++index)
+			if (values[index] > max_value) max_value = values[index];
+	
+		for (size_t index = 0; index < values.size(); ++index)
+			values[i] /= max_value;
+	}
+
+	// DC filter, delay 1
+
+	std::deque<dataType> dcFilter(const std::deque<dataType> & signal)
+	{
+		std::deque<dataType> result;
+		for (size_t index = 0; index < signal.size(); ++index)
+		{
+			dataType value = 0;
+			if (index >= 1) value = signal[index] - signal[index - 1] + 0.995 * result[index - 1];
+			result.push_back(value);
+		}
+		return result;
+	}
+
+	// Low Pass filter, delay 12	6!
+	// Implemented as proposed by the original paper.
+	// y(nT) = 2y(nT - T) - y(nT - 2T) + x(nT) - 2x(nT - 6T) + x(nT - 12T)
+
+	std::deque<dataType> lowPassFilter(const std::deque<dataType> & signal)
+	{
+		std::deque<dataType> result;
+		for (size_t index = 0; index < signal.size(); ++index)
+		{
+			dataType value = signal[index];
+			if (index >= 1) value += 2 * result[index - 1];
+			if (index >= 2) value -= result[index - 2];
+			if (index >= 6) value -= 2 * result[index - 6];
+			if (index >= 12) value += result[index - 12];
+			result.push_back(value);
+		}
+		return result;
+	}
+
+	// High Pass filter, delay 32	16!
+	// Implemented as proposed by the original paper.
+	// y(nT) = 32x(nT - 16T) - [y(nT - T) + x(nT) - x(nT - 32T)]
+
+	std::deque<dataType> highPassFilter(const std::deque<dataType> & signal)
+	{
+		std::deque<dataType> result;
+		for (size_t index = 0; index < signal.size(); ++index)
+		{
+			dataType value = -signal[index];
+			if (index >= 1) value -= result[index - 1];
+			if (index >= 16) value += 32 * result[index - 16];
+			if (index >= 32) value += result[index - 32];
+			result.push_back(value);
+		}
+		return result;
+	}
+
+
+	// Derivative filter, delay 4	5!
+	// Implemented as proposed by the original paper.
+	// y(nT) = (1/8T)[-x(nT - 2T) - 2x(nT - T) + 2x(nT + T) + x(nT + 2T)]
+
+	std::deque<dataType> derivativeFilter(const std::deque<dataType> & signal)
+	{
+		std::deque<dataType> result;
+		for (size_t index = 2; index < signal.size() - 2; ++index)
+		{
+			dataType value = -signal[index - 2] - 2 * signal[index - 1] +
+					2 * signal[index + 1] + signal[index + 2];
+			value /= 8.0;
+			result.push_back(value);
+		}
+		dataType value;
+		value = result.front();
+		result.push_front(value);
+		result.push_front(value);
+		value = result.back();
+		result.push_back(value);
+		result.push_back(value);
+		return result;
+	}
+
+	// Squared filter, delay 0
+	// Implemented as proposed by the original paper.
+	// y(nT) = [x(nT)]^2.
+
+	std::deque<dataType> squaredFilter(const std::deque<dataType> & signal)
+	{
+		std::deque<dataType> result;
+		for (size_t index = 0; index < signal.size(); ++index)
+		{
+			dataType value = signal[index]*signal[index];
+			result.push_back(value);
+		}
+		return result;
+	}
+
+	// Moving-Window Integration, delay N/2 ???
+	// Implemented as proposed by the original paper.
+	// y(nT) = (1/N)[x(nT - (N - 1)T) + x(nT - (N - 2)T) + ... x(nT)]
+	// N, in samples, must be defined so that the window is ~150ms.
+
+	std::deque<dataType> MWI(const std::deque<dataType> & signal, size_t N)
+	{
+		std::deque<dataType> result;
+		dataType value = 0;
+		for (size_t index = 0; index < signal.size(); ++index)
+		{
+			int first = index - (N - 1); //???
+			value += signal[index] / N;
+			if (first > 0) value -= signal[first - 1] / N;
+			result.push_back(value);
+		}
+		return result;
+	}
+
+#endif
--- panTompkins.cpp
+++ panTompkins.cpp
@@ -3,150 +3,20 @@
 #include <stdexcept>
 #include <deque>
 #include "threshold.h"
+#include "filters.h"
 
 typedef int dataType;
 
 #define FS 360	// Sampling frequency.
 
-	struct Filter
+
+	dataType findMax(size_t i, std::deque<dataType> & signal)
 	{
-		std::deque<dataType> process(const std::deque<dataType> & signal) = 0;
-		size_t delay() = 0;
-	};
-
-	struct DCFilter : public Filter	{ };
-	struct LowPassFilter : public Filter { };
-	struct HighPassFilter : public Filter { };
-	struct DerivativeFilter : public Filter { };
-	struct SquaredFilter : public Filter { };
-	struct MWI : public Filter { };
-
-	void normalize(std::deque<dataType> & values)
-	{
-		dataType max_value = values[0];
-		for (size_t index = 1; index < values.size(); ++index)
-			if (values[index] > max_value) max_value = values[index];
-	
-		for (size_t index = 0; index < values.size(); ++index)
-			values[i] /= max_value;
-	}
-
-	// DC filter, delay 1
-
-	std::deque<dataType> dcFilter(const std::deque<dataType> & signal)
-	{
-		std::deque<dataType> result;
-		for (size_t index = 0; index < signal.size(); ++index)
-		{
-			dataType value = 0;
-			if (index >= 1) value = signal[index] - signal[index - 1] + 0.995 * result[index - 1];
-			result.push_back(value);
-		}
-		return result;
-	}
-
-	// Low Pass filter, delay 12	6!
-	// Implemented as proposed by the original paper.
-	// y(nT) = 2y(nT - T) - y(nT - 2T) + x(nT) - 2x(nT - 6T) + x(nT - 12T)
-
-	std::deque<dataType> lowPassFilter(const std::deque<dataType> & signal)
-	{
-		std::deque<dataType> result;
-		for (size_t index = 0; index < signal.size(); ++index)
-		{
-			dataType value = signal[index];
-			if (index >= 1) value += 2 * result[index - 1];
-			if (index >= 2) value -= result[index - 2];
-			if (index >= 6) value -= 2 * result[index - 6];
-			if (index >= 12) value += result[index - 12];
-			result.push_back(value);
-		}
-		return result;
-	}
-
-	// High Pass filter, delay 32	16!
-	// Implemented as proposed by the original paper.
-	// y(nT) = 32x(nT - 16T) - [y(nT - T) + x(nT) - x(nT - 32T)]
-
-	std::deque<dataType> highPassFilter(const std::deque<dataType> & signal)
-	{
-		std::deque<dataType> result;
-		for (size_t index = 0; index < signal.size(); ++index)
-		{
-			dataType value = -signal[index];
-			if (index >= 1) value -= result[index - 1];
-			if (index >= 16) value += 32 * result[index - 16];
-			if (index >= 32) value += result[index - 32];
-			result.push_back(value);
-		}
-		return result;
-	}
-
-
-	// Derivative filter, delay 4	5!
-	// Implemented as proposed by the original paper.
-	// y(nT) = (1/8T)[-x(nT - 2T) - 2x(nT - T) + 2x(nT + T) + x(nT + 2T)]
-
-	std::deque<dataType> derivativeFilter(const std::deque<dataType> & signal)
-	{
-		std::deque<dataType> result;
-		for (size_t index = 2; index < signal.size() - 2; ++index)
-		{
-			dataType value = -signal[index - 2] - 2 * signal[index - 1] +
-					2 * signal[index + 1] + signal[index + 2];
-			value /= 8.0;
-			result.push_back(value);
-		}
-		dataType value;
-		value = result.front();
-		result.push_front(value);
-		result.push_front(value);
-		value = result.back();
-		result.push_back(value);
-		result.push_back(value);
-		return result;
-	}
-
-	// Squared filter, delay 0
-	// Implemented as proposed by the original paper.
-	// y(nT) = [x(nT)]^2.
-
-	std::deque<dataType> squaredFilter(const std::deque<dataType> & signal)
-	{
-		std::deque<dataType> result;
-		for (size_t index = 0; index < signal.size(); ++index)
-		{
-			dataType value = signal[index]*signal[index];
-			result.push_back(value);
-		}
-		return result;
-	}
-
-	// Moving-Window Integration, delay N/2 ???
-	// Implemented as proposed by the original paper.
-	// y(nT) = (1/N)[x(nT - (N - 1)T) + x(nT - (N - 2)T) + ... x(nT)]
-	// N, in samples, must be defined so that the window is ~150ms.
-
-	std::deque<dataType> MWI(const std::deque<dataType> & signal, size_t N)
-	{
-		std::deque<dataType> result;
-		dataType value = 0;
-		for (size_t index = 0; index < signal.size(); ++index)
-		{
-			int first = index - (N - 1); //???
-			value += signal[index] / N;
-			if (first > 0) value -= signal[first - 1] / N;
-			result.push_back(value);
-		}
-		return result;
-	}
-
-	dataType slope(size_t i, std::deque<dataType> & squared)
-	{
-		dataType currentSlope = 0;
-		for (size_t j = i - 10; j <= i; j++)
-			if (squared[j] > currentSlope) currentSlope = squared[j];
-		return currentSlope;
+		assert(i>=10);
+		dataType max_value = 0;
+		for (size_t j = i - 10; j <= i; ++j)
+			if (signal[j] > max_value) max_value = signal[j];
+		return max_value;
 	}
 
 	struct panTompkins
@@ -170,7 +40,7 @@ typedef int dataType;
 
 		bool regular = true;
 
-		std::list<std::unique_ptr<Filter>> filters;
+		//std::list<std::unique_ptr<Filter>> filters;
 	public:
 
 	explicit panTompkins(size_t fs) :
@@ -180,12 +50,12 @@ typedef int dataType;
 		rrmax(0.36*fs),
 		
 	{
-		filters.push_back(std::make_unique<DCFilter>);
-		filters.push_back(std::make_unique<LowPassFilter>);
-		filters.push_back(std::make_unique<HighPassFilter>);
-		filters.push_back(std::make_unique<DerivativeFilter>);
-		filters.push_back(std::make_unique<SquaredFilter>);
-		filters.push_back(std::make_unique<MWI>);
+		//filters.push_back(std::make_unique<DCFilter>);
+		//filters.push_back(std::make_unique<LowPassFilter>);
+		//filters.push_back(std::make_unique<HighPassFilter>);
+		//filters.push_back(std::make_unique<DerivativeFilter>);
+		//filters.push_back(std::make_unique<SquaredFilter>);
+		//filters.push_back(std::make_unique<MWI>);
 	}
 
 	std::deque<bool> detect(const std::deque<dataType> & signal);
@@ -292,7 +162,7 @@ std::deque<bool> panTompkins::detect(const std::deque<dataType> & signal)
 		{
 			if (sample - lastQRS > rrmin)
 			{
-				dataType currentSlope = slope(current, squared);
+				dataType currentSlope = findMax(current, squared);
 				if (sample - lastQRS <= rrmax)
 				{
 					if (currentSlope <= lastSlope/2)
@@ -341,7 +211,7 @@ std::deque<bool> panTompkins::detect(const std::deque<dataType> & signal)
 					if ( (integral[i] > threshold_i.i2) &&
 					     (bandpass[i] > threshold_f.i2))
 					{
-						dataType currentSlope = slope(i, squared);
+						dataType currentSlope = findMax(i, squared);
 						if (
 							(currentSlope < lastSlope/2) &&
 							(i + sample) < lastQRS + 0.36*lastQRS)
